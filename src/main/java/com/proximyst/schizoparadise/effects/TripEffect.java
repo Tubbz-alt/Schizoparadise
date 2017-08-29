de7b424dd8e48344d8600d0ba13ae60a24c1fa20
Mariell Hoversholm <proximyst@proximyst.com>
2017-08-29 16:02:34 +0200

    Track trippers

--- src/main/java/com/proximyst/schizoparadise/effects/TripEffect.java
+++ src/main/java/com/proximyst/schizoparadise/effects/TripEffect.java
@@ -5,10 +5,14 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TripEffect extends Effect {
     private final TaskChainFactory factory;
+    private final Set<UUID> ineffect = new HashSet<>();
 
     public TripEffect(TaskChainFactory factory) {
         super("Trip", 20);
@@ -22,14 +26,18 @@ public class TripEffect extends Effect {
         double z = ThreadLocalRandom.current().nextDouble(5 * 2) - 5;
         factory.newChain()
                 .syncFirst(() -> {
+                    ineffect.add(player.getUniqueId());
                     Location location = player.getLocation();
                     player.getVelocity().add(new Vector(x, y, z));
                     return location;
                 })
                 .delay(5)
                 .syncLast((loc) -> {
-                    if (player != null && player.isOnline())
-                        player.teleport(loc);
+                    if (player != null) {
+                        ineffect.remove(player.getUniqueId());
+                        if (player.isOnline())
+                            player.teleport(loc);
+                    }
                 })
                 .execute();
     }
